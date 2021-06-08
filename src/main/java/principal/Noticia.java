5b00b14249958ee8d40f64d7371d01de90496ac0
Osnir Basílio <osnirxadrez@gmail.com>
2021-06-08 19:41:25 -0300

    Finalizando a formatação na data e hora e inserindo comentários no código

--- src/main/java/principal/Noticia.java
+++ src/main/java/principal/Noticia.java
@@ -20,8 +20,63 @@ public class Noticia {
 	private String autor;
 	private LocalDateTime dataPublicacao;
 	private String conteudo;
-	
-	public void retiraInformacoes(String url){
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");	
+    
+	//*****************MÉTODOS*********************
+    
+    //Método que retira o titulo da pagina selecionada
+    public String retiraTitulo(Document document, String caminhoTitulo) {
+    	Element elementTitulo = document.select(caminhoTitulo).first();
+		String titulo = elementTitulo.select("h1").text();
+		
+		return titulo;
+    }
+    
+    //Método que retira o subtitulo da pagina selecionada
+    public String retiraSubTitulo(Document document, String caminhoSubTitulo) {
+    	Element elementSubTitulo = document.select(caminhoSubTitulo).first();
+		String subTitulo = elementSubTitulo.text();
+		
+		return subTitulo;
+    }
+    
+    //Método que retira o autor da pagina selecionada
+    public String retiraAutor(Document document, String caminhoAutor) {
+    	Element elementAutor = document.select(caminhoAutor).first();
+		String autor = elementAutor.select("a").text();
+		
+		return autor;
+    }
+    
+    //Método que retira a data da pagina selecioada e prepara para conversão para o formato desejado
+    public static LocalDateTime retiraDataPublicacao(Document document, String caminhoDataPublicacao) {
+    	Element elementDataPublicacao = document.select(caminhoDataPublicacao).first();
+		String dataPublicacao = elementDataPublicacao.attr("datetime");
+		return LocalDateTime.parse(dataPublicacao, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+    
+    // Método que retira o conteúdo da pagina selecionada
+    public String retiraConteudo(Document document, String caminhoConteudo) {
+    	Element elementConteudo = document.select(caminhoConteudo).first();
+		String conteudo = elementConteudo.select("p").text();
+		
+		return conteudo;
+    }
+    
+    // Método que faz o POST na pagina para carregar mais noticias
+    public static List<String> getUrlsUltimasNoticiasMercados(int pagina, String urlPrincipal) {
+        HttpResponse<JsonNode> httpResponse = Unirest
+                .post(urlPrincipal + "?infinity=scrolling")
+                .field("action", "infinite_scroll")
+                .field("page", String.valueOf(pagina))
+                .field("order", "DESC")
+                .asJson();
+        String postflairs = httpResponse.getBody().getObject().get("postflair").toString();
+        return List.copyOf(new JSONObject(postflairs).keySet());
+    }
+    
+    // Retira as informações da pagina selecionada
+    public void retiraInformacoes(String url){
 		this.url = url;
 		Document document = null;
 		try {
@@ -35,6 +90,18 @@ public class Noticia {
 			
 		}
 	}
+    
+    //toString
+	@Override
+	public String toString() {
+		return "Noticia [url="+url+", titulo=" + titulo + ", subtitulo=" + subtitulo + ", autor=" + autor
+				+ ", dataPublicacao=" + DATE_TIME_FORMATTER.format(dataPublicacao) + ", conteudo=" + conteudo + "]";
+	}
+	
+	//Construtores
+	public Noticia() {
+		super();
+	}	
 	
 	public Noticia(String url, String titulo, String subtitulo, String autor, LocalDateTime dataPublicacao,
 			String conteudo) {
@@ -46,108 +113,4 @@ public class Noticia {
 		this.dataPublicacao = dataPublicacao;
 		this.conteudo = conteudo;
 	}
-
-	//Métodos
-    public String retiraTitulo(Document document, String caminhoTitulo) {
-    	Element elementTitulo = document.select(caminhoTitulo).first();
-		String titulo = elementTitulo.select("h1").text();
-		
-		return titulo;
-    }
-    public String retiraSubTitulo(Document document, String caminhoSubTitulo) {
-    	Element elementSubTitulo = document.select(caminhoSubTitulo).first();
-		String subTitulo = elementSubTitulo.text();
-		
-		return subTitulo;
-    }
-    public String retiraAutor(Document document, String caminhoAutor) {
-    	Element elementAutor = document.select(caminhoAutor).first();
-		String autor = elementAutor.select("a").text();
-		
-		return autor;
-    }
-    public static LocalDateTime retiraDataPublicacao(Document document, String caminhoDataPublicacao) {
-    	Element elementDataPublicacao = document.select(caminhoDataPublicacao).first();
-		String dataPublicacao = elementDataPublicacao.attr("datetime");
-		return LocalDateTime.parse(dataPublicacao, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-    }
-    public String retiraConteudo(Document document, String caminhoConteudo) {
-    	Element elementConteudo = document.select(caminhoConteudo).first();
-		String conteudo = elementConteudo.select("p").text();
-		
-		return conteudo;
-    }
-    public static List<String> getUrlsUltimasNoticiasMercados(int pagina, String urlPrincipal) {
-        HttpResponse<JsonNode> httpResponse = Unirest
-                .post(urlPrincipal + "?infinity=scrolling")
-                .field("action", "infinite_scroll")
-                .field("page", String.valueOf(pagina))
-                .field("order", "DESC")
-                .asJson();
-        String postflairs = httpResponse.getBody().getObject().get("postflair").toString();
-        return List.copyOf(new JSONObject(postflairs).keySet());
-    }
-
-	
-	
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
-		result = prime * result + ((conteudo == null) ? 0 : conteudo.hashCode());
-		result = prime * result + ((dataPublicacao == null) ? 0 : dataPublicacao.hashCode());
-		result = prime * result + ((subtitulo == null) ? 0 : subtitulo.hashCode());
-		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
-		result = prime * result + ((url == null) ? 0 : url.hashCode());
-		return result;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "Noticia [url="+url+", titulo=" + titulo + ", subtitulo=" + subtitulo + ", autor=" + autor
-				+ ", dataPublicacao=" + dataPublicacao + ", conteudo=" + conteudo + "]";
-	}
-	public Noticia() {
-		super();
-	}	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Noticia other = (Noticia) obj;
-		if (autor == null) {
-			if (other.autor != null)
-				return false;
-		} else if (!autor.equals(other.autor))
-			return false;
-		if (conteudo == null) {
-			if (other.conteudo != null)
-				return false;
-		} else if (!conteudo.equals(other.conteudo))
-			return false;
-		if (dataPublicacao == null) {
-			if (other.dataPublicacao != null)
-				return false;
-		} else if (!dataPublicacao.equals(other.dataPublicacao))
-			return false;
-		if (subtitulo == null) {
-			if (other.subtitulo != null)
-				return false;
-		} else if (!subtitulo.equals(other.subtitulo))
-			return false;
-		if (titulo == null) {
-			if (other.titulo != null)
-				return false;
-		} else if (!titulo.equals(other.titulo))
-			return false;
-		return true;
-	}	
 }
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -12,11 +12,14 @@ public class Principal {
 		 
 		 try {					 
 			   List<String> urlsUltimasNoticiasMercados = new ArrayList<>();
+			   //Chamada do método para fazer os POSTs na pagina para que ela retorne novas noticias, 3 vezes, 1 para cada pagina
 	            urlsUltimasNoticiasMercados.addAll(Noticia.getUrlsUltimasNoticiasMercados(1, urlPrincipal));
 	            urlsUltimasNoticiasMercados.addAll(Noticia.getUrlsUltimasNoticiasMercados(2, urlPrincipal));
 	            urlsUltimasNoticiasMercados.addAll(Noticia.getUrlsUltimasNoticiasMercados(3, urlPrincipal));
 
 	            System.out.println("Últimas Notícias");
+	            
+	            //For para chamar o método que retira as informações de cada URL e imprimir no console
 	            for (String url : urlsUltimasNoticiasMercados) {
 	                noticia.retiraInformacoes(url);
 	                System.out.println(noticia.toString());
